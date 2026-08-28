5a6e831362a25ae35f9fd6a60d73e357accb5230
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-08-28 18:54:41 +0300

    fix header file

--- include/device_matrix.hpp
+++ include/device_matrix.hpp
@@ -1,15 +1,16 @@
 #ifndef DEVICE_MATRIX_H
 #define DEVICE_MATRIX_H
+#include <matrix.hpp>
 class DeviceMatrix{
     public:
         int rows, cols;
         float *data;
+        
         DeviceMatrix(int, int);
         ~DeviceMatrix();
 
-        void copy_to_host(void *);
-        void copy_from_host(void *);
-
+        void copy_to_host(const Matrix&);
+        void copy_from_host(const Matrix&);
 
 };
 #endif

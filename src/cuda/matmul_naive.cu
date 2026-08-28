55547910e0b1fe816d520384d24afa416ed8d7d2
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-08-28 17:19:04 +0300

    implement naive matmul

---
+++ src/cuda/matmul_naive.cu
@@ -0,0 +1,16 @@
+#include <device_matrix.hpp>
+__global__ void naive_matmul(float *out, float *left, float *right, int M, int K, int N){
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (idx > M * N - 1){
+        return;
+    }
+
+    int row = idx / N;
+    int col = idx % N;
+    float sum = 0.0f;
+    for (int k = 0; k < K; k++){
+        sum += left[k + (row * K)] * right[(k*N) + col];
+    }
+    out[idx] = sum;
+}

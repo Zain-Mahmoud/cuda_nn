9e1750948793d46b8ba80ad888b680179c3b2391
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-08-31 18:55:26 +0300

    include TILESIZE constant

--- include/matmul.cuh
+++ include/matmul.cuh
@@ -1,5 +1,8 @@
 #ifndef MATMUL_KERNEL_H
 #define MATMUL_KERNEL_H
+
+#define TILE_SIZE 4
 __global__ void naive_matmul_kernel(float *out, float *left, float *right, int M, int K, int N);
 __global__ void tiled_matmul_kernel(float *out, float *left, float *right, int M, int K, int N);
+
 #endif

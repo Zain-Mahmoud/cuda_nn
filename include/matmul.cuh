7b511d94d446be5290a38856a777da74bf746a11
Zain-Mahmoud <zain.mahmoud@mail.utoronto.ca>
2026-08-31 17:26:35 +0300

    update gpu matmul cuh file

--- include/matmul.cuh
+++ include/matmul.cuh
@@ -1,6 +1,5 @@
 #ifndef MATMUL_KERNEL_H
 #define MATMUL_KERNEL_H
-
 __global__ void naive_matmul_kernel(float *out, float *left, float *right, int M, int K, int N);
-
+__global__ void tiled_matmul_kernel(float *out, float *left, float *right, int M, int K, int N);
 #endif
